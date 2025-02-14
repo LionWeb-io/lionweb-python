832926efa6cdc93a9d814f17ff90cc232b4f79bf
Federico Tomassetti <federico@strumenta.com>
2025-02-14 10:28:15 +0100

    Translating LanguageEntity

--- src/lionwebpython/language/language.py
+++ src/lionwebpython/language/language.py
@@ -4,4 +4,5 @@ from lionwebpython.model.impl.m3node import M3Node
 
 
 class Language(M3Node["Language"], NamespaceProvider, IKeyed["Language"]):
-    pass
+    def add_element(self, self1):
+        raise ValueError("NOT YET IMPLEMENTED")
