fdbde1b188ec993acf27b83ce764b9f271c67478
ftomassetti <federico@strumenta.com>
2025-03-17 19:13:22 +0100

    Move to bump2version

---
+++ release.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e  # Stop on errors
+
+# Bump version
+bump2version patch  # Change to minor or major if needed
+
+# Push changes
+git push origin main --tags
+
+# Build the package
+python -m build
+
+# Upload to PyPI
+twine upload dist/*
