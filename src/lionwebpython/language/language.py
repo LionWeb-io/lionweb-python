f930d29eef6900240e63c0a0c166735a9e5c896d
ftomassetti <federico@strumenta.com>
2025-03-23 09:46:16 +0100

    Adding property for Language.version

--- src/lionwebpython/language/language.py
+++ src/lionwebpython/language/language.py
@@ -60,6 +60,14 @@ class Language(M3Node["Language"], NamespaceProvider, IKeyed["Language"]):
     def key(self, new_value):
         self.set_property_value(property_name="key", value=new_value)
 
+    @property
+    def version(self):
+        return cast(str, self.get_property_value(property_name="version"))
+
+    @version.setter
+    def version(self, new_value):
+        self.set_property_value(property_name="version", value=new_value)
+
     def namespace_qualifier(self) -> str:
         name = self.get_name()
         if name:
