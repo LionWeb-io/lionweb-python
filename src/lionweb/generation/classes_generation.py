6fd34a3a1b1fee049d5c7eb0e5d8277d95d23898
Federico Tomassetti <federico@strumenta.com>
2025-05-02 12:27:36 +0200

    Initial porting of the generation logic

---
+++ src/lionweb/generation/classes_generation.py
@@ -0,0 +1,249 @@
+import ast
+import keyword
+from _ast import ClassDef
+from pathlib import Path
+from typing import List, Dict
+
+import astor
+from lionweb.language import Language, Concept, Interface, Containment, Property, Feature
+from lionweb.language.classifier import Classifier
+from lionweb.language.enumeration import Enumeration
+from lionweb.language.primitive_type import PrimitiveType
+from lionweb.language.reference import Reference
+from lionweb.lionweb_version import LionWebVersion
+
+from pylasu.lionweb.starlasu import StarLasuBaseLanguage
+from pylasu.lionweb.utils import calculate_field_name
+
+
+def _identify_topological_deps(classifiers: List[Classifier], id_to_concept) -> Dict[str, List[str]]:
+    graph: Dict[str, List[str]] = {el.get_id(): [] for el in classifiers}
+    for c in classifiers:
+        if isinstance(c, Concept):
+            if c.get_extended_concept() and c.get_extended_concept().get_id() in id_to_concept:
+                graph[c.get_id()].append(c.get_extended_concept().get_id())
+            for i in c.get_implemented():
+                graph[c.get_id()].append(i.get_id())
+            for f in c.get_features():
+                if isinstance(f, Containment):
+                    if f.get_type() and f.get_type().get_id() in id_to_concept:
+                        graph[c.get_id()].append(f.get_type().get_id())
+        elif isinstance(c, Interface):
+            pass
+        else:
+            raise ValueError()
+    return graph
+
+
+def topological_classifiers_sort(classifiers: List[Classifier]) -> List[Classifier]:
+    id_to_concept = {el.get_id(): el for el in classifiers}
+
+    # Build graph edges: child -> [parents]
+    graph: Dict[str, List[str]] = _identify_topological_deps(classifiers, id_to_concept)
+
+    visited = set()
+    sorted_list = []
+
+    def visit(name: str):
+        if name in visited:
+            return
+        visited.add(name)
+        if name in graph:
+            for dep in graph[name]:
+                visit(dep)
+        if name in id_to_concept:
+            sorted_list.append(id_to_concept[name])
+
+    for c in classifiers:
+        visit(c.get_id())
+
+    return sorted_list
+
+
+def _generate_from_containment(feature: Containment, classdef: ClassDef):
+    field_name = calculate_field_name(feature)
+    type = feature.get_type().get_name()
+    if feature.is_multiple():
+        type = f"List[{type}]"
+    elif feature.is_optional():
+        type = f"Optional[{type}]"
+    field = ast.AnnAssign(
+        target=ast.Name(id=field_name, ctx=ast.Store()),
+        annotation=ast.Constant(value=type),
+        value=None,
+        simple=1,
+    )
+    if len(classdef.body) == 1 and isinstance(classdef.body[0], ast.Pass):
+        classdef.body = []
+    classdef.body.append(field)
+
+
+def _generate_from_feature(feature: Feature, classdef: ClassDef):
+    if isinstance(feature, Containment):
+        _generate_from_containment(feature, classdef)
+    elif isinstance(feature, Reference):
+        field_name = feature.get_name()
+        if field_name in keyword.kwlist:
+            field_name = f"{field_name}_"
+        type = f"ReferenceByName[{feature.get_type().get_name()}]"
+        if feature.is_optional():
+            type = f"Optional[{type}]"
+        field = ast.AnnAssign(
+            target=ast.Name(id=field_name, ctx=ast.Store()),
+            annotation=ast.Constant(value=type),
+            value=None,
+            simple=1,
+        )
+        if len(classdef.body) == 1 and isinstance(classdef.body[0], ast.Pass):
+            classdef.body = []
+        classdef.body.append(field)
+    elif isinstance(feature, Property):
+        field_name = feature.get_name()
+        if field_name in keyword.kwlist:
+            field_name = f"{field_name}_"
+        type = feature.get_type().get_name()
+        if feature.is_optional():
+            type = f"Optional[{type}]"
+        field = ast.AnnAssign(
+            target=ast.Name(id=field_name, ctx=ast.Store()),
+            annotation=ast.Constant(value=type),
+            value=None,
+            simple=1,
+        )
+        if len(classdef.body) == 1 and isinstance(classdef.body[0], ast.Pass):
+            classdef.body = []
+        classdef.body.append(field)
+    else:
+        raise ValueError()
+
+
+def _generate_from_concept(classifier: Concept) -> ClassDef:
+    bases = []
+    if classifier.get_extended_concept().id == StarLasuBaseLanguage.get_astnode(LionWebVersion.V2023_1).id:
+        if len(classifier.get_implemented()) == 0:
+            bases.append('Node')
+    else:
+        bases.append(classifier.get_extended_concept().get_name())
+    special_interfaces = {
+        'com-strumenta-StarLasu-Expression-id': 'StarLasuExpression',
+        'com-strumenta-StarLasu-Statement-id': 'StarLasuStatement',
+        'com-strumenta-StarLasu-PlaceholderElement-id': 'StarLasuPlaceholderElement',
+        'com-strumenta-StarLasu-Parameter-id': 'StarLasuParameter',
+        'com-strumenta-StarLasu-Documentation-id': 'StarLasuDocumentation',
+        'com-strumenta-StarLasu-BehaviorDeclaration-id': 'StarLasuBehaviorDeclaration',
+        'com-strumenta-StarLasu-EntityDeclaration-id': 'StarLasuEntityDeclaration',
+        'LionCore-builtins-INamed': 'StarLasuNamed'
+    }
+    for i in classifier.get_implemented():
+        if i.get_id() in special_interfaces:
+            bases.append(special_interfaces[i.get_id()])
+        else:
+            bases.append(i.get_name())
+    # if classifier.is_abstract():
+    #    bases.append('ABC')
+    dataclass_decorator = ast.Name(id="dataclass", ctx=ast.Load())
+    classdef = ast.ClassDef(classifier.get_name(), bases=bases,
+                            keywords=[],
+                            body=[ast.Pass()],
+                            decorator_list=[dataclass_decorator])
+
+    for feature in classifier.get_features():
+        _generate_from_feature(feature, classdef)
+
+    return classdef
+
+
+def classes_generation(click, language: Language, output):
+    import_abc = ast.ImportFrom(
+        module='abc',
+        names=[ast.alias(name='ABC', asname=None)],
+        level=0
+    )
+    import_dataclass = ast.ImportFrom(
+        module='dataclasses',
+        names=[ast.alias(name='dataclass', asname=None)],
+        level=0
+    )
+    import_enum = ast.ImportFrom(
+        module="enum",
+        names=[ast.alias(name="Enum", asname=None)],
+        level=0
+    )
+    import_typing = ast.ImportFrom(
+        module='typing',
+        names=[ast.alias(name='Optional', asname=None)],
+        level=0
+    )
+    import_starlasu = ast.ImportFrom(
+        module='pylasu.model.metamodel',
+        names=[ast.alias(name='Expression', asname='StarLasuExpression'),
+               ast.alias(name='PlaceholderElement', asname='StarLasuPlaceholderElement'),
+               ast.alias(name='Named', asname='StarLasuNamed'),
+               ast.alias(name='TypeAnnotation', asname='StarLasuTypeAnnotation'),
+               ast.alias(name='Parameter', asname='StarLasuParameter'),
+               ast.alias(name='Statement', asname='StarLasuStatement'),
+               ast.alias(name='EntityDeclaration', asname='StarLasuEntityDeclaration'),
+               ast.alias(name='BehaviorDeclaration', asname='StarLasuBehaviorDeclaration'),
+               ast.alias(name='Documentation', asname='StarLasuDocumentation')],
+        level=0
+    )
+    import_node = ast.ImportFrom(
+        module='pylasu.model',
+        names=[ast.alias(name='Node', asname=None)],
+        level=0
+    )
+    module = ast.Module(body=[import_abc, import_dataclass, import_typing, import_enum, import_starlasu, import_node],
+                        type_ignores=[])
+
+    for element in language.get_elements():
+        if isinstance(element, Concept):
+            pass
+        elif isinstance(element, Interface):
+            pass
+        elif isinstance(element, PrimitiveType):
+            pass
+        elif isinstance(element, Enumeration):
+            members = [
+                ast.Assign(
+                    targets=[ast.Name(id=literal.get_name(), ctx=ast.Store())],
+                    value=ast.Constant(value=literal.get_name())
+                )
+                for literal in element.get_literals()
+            ]
+
+            enum_class = ast.ClassDef(
+                name=element.get_name(),
+                bases=[ast.Name(id="Enum", ctx=ast.Load())],
+                keywords=[],
+                body=members,
+                decorator_list=[]
+            )
+            module.body.append(enum_class)
+        else:
+            raise ValueError(f"Unsupported {element}")
+
+    sorted_classifier = topological_classifiers_sort([c for c in language.get_elements() if isinstance(c, Classifier)])
+
+    for classifier in sorted_classifier:
+        if isinstance(classifier, Concept):
+            module.body.append(_generate_from_concept(classifier))
+        elif isinstance(classifier, Interface):
+            bases = []
+            if len(classifier.get_extended_interfaces()) == 0:
+                bases.append("Node")
+                # bases.append("ABC")
+
+            classdef = ast.ClassDef(classifier.get_name(), bases=bases,
+                                    keywords=[],
+                                    body=[ast.Pass()],
+                                    decorator_list=[])
+            module.body.append(classdef)
+        else:
+            raise ValueError()
+
+    click.echo(f"📂 Saving ast to: {output}")
+    generated_code = astor.to_source(module)
+    output_path = Path(output)
+    output_path.mkdir(parents=True, exist_ok=True)
+    with Path(f"{output}/ast.py").open("w", encoding="utf-8") as f:
+        f.write(generated_code)
---
+++ src/lionweb/generation/deserializer_generation.py
@@ -0,0 +1,259 @@
+import ast
+from pathlib import Path
+
+import astor
+from lionweb.language import Language, Concept
+from lionweb.language.enumeration import Enumeration
+from lionweb.language.primitive_type import PrimitiveType
+
+from pylasu.lionweb.utils import to_snake_case, calculate_field_name
+
+
+def make_cond(enumeration_name: str, member_name: str):
+    return ast.Compare(
+        left=ast.Name(id="serialized", ctx=ast.Load()),
+        ops=[ast.Eq()],
+        comparators=[
+            ast.Attribute(
+                value=ast.Attribute(
+                    value=ast.Name(id=enumeration_name, ctx=ast.Load()),
+                    attr=member_name,
+                    ctx=ast.Load()
+                ),
+                attr="value",
+                ctx=ast.Load()
+            )
+        ]
+    )
+
+
+# The return: return AssignmentType.Add
+def make_return(enumeration_name: str, member_name: str):
+    return ast.Return(
+        value=ast.Attribute(
+            value=ast.Name(id=enumeration_name, ctx=ast.Load()),
+            attr=member_name,
+            ctx=ast.Load()
+        )
+    )
+
+
+def generate_register_deserializers_func(language: Language) -> ast.FunctionDef:
+    fd = ast.FunctionDef(
+        name="register_deserializers",
+        args=ast.arguments(
+            posonlyargs=[],
+            args=[
+                ast.arg(arg="json_serialization", annotation=ast.Name(id="JsonSerialization", ctx=ast.Load()))
+            ],
+            kwonlyargs=[],
+            kw_defaults=[],
+            defaults=[]
+        ),
+        body=[
+        ],
+        decorator_list=[],
+        returns=None
+    )
+    for e in language.get_elements():
+        if isinstance(e, Enumeration):
+            fd.body.append(ast.Expr(
+                value=ast.Call(
+                    func=ast.Attribute(
+                        value=ast.Attribute(
+                            value=ast.Name(id="json_serialization", ctx=ast.Load()),
+                            attr="primitive_values_serialization",
+                            ctx=ast.Load()
+                        ),
+                        attr="register_deserializer",
+                        ctx=ast.Load()
+                    ),
+                    args=[
+                        ast.Constant(value=e.get_id()),
+                        ast.Name(id=f"_deserialize_{to_snake_case(e.get_name())}", ctx=ast.Load())
+                    ],
+                    keywords=[]
+                )
+            ))
+        elif isinstance(e, Concept):
+            fd.body.append(ast.Expr(
+                value=ast.Call(
+                    func=ast.Attribute(
+                        value=ast.Attribute(
+                            value=ast.Name(id="json_serialization", ctx=ast.Load()),
+                            attr="instantiator",
+                            ctx=ast.Load()
+                        ),
+                        attr="register_custom_deserializer",
+                        ctx=ast.Load()
+                    ),
+                    args=[
+                        ast.Constant(value=e.get_id()),
+                        ast.Name(id=f"_deserialize_{to_snake_case(e.get_name())}", ctx=ast.Load())
+                    ],
+                    keywords=[]
+                )
+            ))
+    return fd
+
+
+def generate_concept_deserializer(concept: Concept) -> ast.FunctionDef:
+    constructor_assignments = []
+    for f in concept.all_features():
+        field_name = calculate_field_name(f)
+        constructor_assignments.append(ast.keyword(arg=field_name, value=ast.Constant(value=f.get_name())))
+    return ast.FunctionDef(
+        name=f"_deserialize_{to_snake_case(concept.get_name())}",
+        args=ast.arguments(
+            posonlyargs=[],
+            args=[
+                ast.arg(arg="classifier"),
+                ast.arg(arg="serialized_instance"),
+                ast.arg(arg="deserialized_instances_by_id"),
+                ast.arg(arg="properties_values")
+            ],
+            kwonlyargs=[],
+            kw_defaults=[],
+            defaults=[]
+        ),
+        body=[
+            ast.Return(
+                value=ast.Call(
+                    func=ast.Name(id=concept.get_name(), ctx=ast.Load()),
+                    args=[],
+                    keywords=constructor_assignments
+                )
+            )
+        ],
+        decorator_list=[],
+        returns=ast.Name(id=concept.get_name(), ctx=ast.Load())
+    )
+
+
+def deserializer_generation(click, language: Language, output):
+    import_abc = ast.ImportFrom(
+        module='abc',
+        names=[ast.alias(name='ABC', asname=None)],
+        level=0
+    )
+    import_dataclass = ast.ImportFrom(
+        module='dataclasses',
+        names=[ast.alias(name='dataclass', asname=None)],
+        level=0
+    )
+    import_enum = ast.ImportFrom(
+        module="enum",
+        names=[ast.alias(name="Enum", asname=None)],
+        level=0
+    )
+    import_typing = ast.ImportFrom(
+        module='typing',
+        names=[ast.alias(name='Optional', asname=None)],
+        level=0
+    )
+    import_starlasu = ast.ImportFrom(
+        module='pylasu.model.metamodel',
+        names=[ast.alias(name='Expression', asname='StarLasuExpression'),
+               ast.alias(name='PlaceholderElement', asname='StarLasuPlaceholderElement'),
+               ast.alias(name='Named', asname='StarLasuNamed'),
+               ast.alias(name='TypeAnnotation', asname='StarLasuTypeAnnotation'),
+               ast.alias(name='Parameter', asname='StarLasuParameter'),
+               ast.alias(name='Statement', asname='StarLasuStatement'),
+               ast.alias(name='EntityDeclaration', asname='StarLasuEntityDeclaration'),
+               ast.alias(name='BehaviorDeclaration', asname='StarLasuBehaviorDeclaration'),
+               ast.alias(name='Documentation', asname='StarLasuDocumentation')],
+        level=0
+    )
+    import_node = ast.ImportFrom(
+        module='pylasu.model',
+        names=[ast.alias(name='Node', asname=None)],
+        level=0
+    )
+    import_ast = ast.ImportFrom(
+        module='.ast',
+        names=[ast.alias(name=e.get_name(), asname=None) for e in language.get_elements()
+               if not isinstance(e, PrimitiveType)],
+        level=0
+    )
+    import_primitives = ast.ImportFrom(
+        module='primitive_types',
+        names=[ast.alias(name=e.get_name(), asname=None) for e in language.get_elements()
+               if isinstance(e, PrimitiveType)],
+        level=0
+    )
+    import_json_serialization = ast.ImportFrom(
+        module='lionwebpython.serialization.json_serialization',
+        names=[ast.alias(name='JsonSerialization', asname=None)],
+        level=0
+    )
+    module = ast.Module(body=[import_abc, import_dataclass, import_typing, import_enum, import_starlasu, import_node,
+                              import_ast, import_primitives, import_json_serialization],
+                        type_ignores=[])
+
+    for e in language.get_elements():
+        if isinstance(e, Enumeration):
+            arg_serialized = ast.arg(arg="serialized", annotation=ast.Name(id="str", ctx=ast.Load()))
+            # The raise: raise ValueError(f"...")
+            raise_stmt = ast.Raise(
+                exc=ast.Call(
+                    func=ast.Name(id="ValueError", ctx=ast.Load()),
+                    args=[
+                        ast.JoinedStr(values=[
+                            ast.Constant(value=f"Invalid value for {e.get_name()}: "),
+                            ast.FormattedValue(
+                                value=ast.Name(id="serialized", ctx=ast.Load()),
+                                conversion=-1
+                            )
+                        ])
+                    ],
+                    keywords=[]
+                ),
+                cause=None
+            )
+            # The function body
+            literals = e.get_literals()
+            current_if = ast.If(
+                test=make_cond(e.get_name(), literals[0].get_name()),
+                body=[make_return(e.get_name(), literals[0].get_name())],
+                orelse=[]
+            )
+            root_if = current_if
+
+            for literal in literals[1:]:
+                next_if = ast.If(
+                    test=make_cond(e.get_name(), literal.get_name()),
+                    body=[make_return(e.get_name(), literal.get_name())],
+                    orelse=[]
+                )
+                current_if.orelse = [next_if]
+                current_if = next_if
+
+            # Final else
+            current_if.orelse = [raise_stmt]
+
+            # Function definition
+            func_def = ast.FunctionDef(
+                name=f"_deserialize_{to_snake_case(e.get_name())}",
+                args=ast.arguments(
+                    posonlyargs=[],
+                    args=[arg_serialized],
+                    kwonlyargs=[],
+                    kw_defaults=[],
+                    defaults=[]
+                ),
+                body=[root_if],
+                decorator_list=[],
+                returns=ast.Constant(value=e.get_name())
+            )
+            module.body.append(func_def)
+        elif isinstance(e, Concept):
+            module.body.append(generate_concept_deserializer(e))
+
+    module.body.append(generate_register_deserializers_func(language))
+
+    generated_code = astor.to_source(module)
+    output_path = Path(output)
+    output_path.mkdir(parents=True, exist_ok=True)
+    click.echo(f"📂 Saving deserializer to: {output}")
+    with Path(f"{output}/deserializer.py").open("w", encoding="utf-8") as f:
+        f.write(generated_code)
