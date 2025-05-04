046ec93e03c66355bdac58d76f98dbc460436726
ftomassetti <federico@strumenta.com>
2025-05-04 18:05:31 +0200

    Pythonize issue_severity.py

--- src/lionweb/utils/__init__.py
+++ src/lionweb/utils/__init__.py
@@ -1,3 +1,5 @@
 from .id_utils import clean_string_as_id, is_valid_id
+from .issue import Issue
+from .issue_severity import IssueSeverity
 
-__all__ = ["is_valid_id", "clean_string_as_id"]
+__all__ = ["is_valid_id", "clean_string_as_id", "Issue", "IssueSeverity"]
